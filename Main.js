aae358b82c2effe9e8123bb86ca44fd7edb32d0c
lin-zhou <zhou.lindsay@yahoo.com>
2019-04-17 18:38:21 -0400

    Can create characters with Sprites

--- Main.js
+++ Main.js
@@ -1,8 +1,12 @@
 const app = new PIXI.Application(1024 * .85, 576 * .85);
 document.body.appendChild(app.view);
 
-// Note: This is for testing how to work with Google Chrome. Will be deleted.
+// Testing how to work with Google Chrome. Will be deleted.
 const howToBackground = new PIXI.Sprite.from("images/backgrounds/How_To_Background.png");
 howToBackground.scale.x = 0.655;
 howToBackground.scale.y = 0.655;
 app.stage.addChild(howToBackground);
+
+// Testing characters sprites.
+var chara = new Character("hannit");
+app.stage.addChild(chara.getSprite());
--- game-setup/Character.js
+++ game-setup/Character.js
@@ -1,16 +1,58 @@
-export class Character {
-    constructor(sprite) {
-        this.sprite = sprite;
-        
-        // Switch with x and y modifiers based on sprite
-    }
-}
+class Character {
 
-export const alfyn = PIXI.Texture.from("images/character-sprites/Alfyn_Sprite.png");
-export const cyrus = PIXI.Texture.from("images/character-sprites/Cyrus_Sprite.png");
-export const hannit = PIXI.Texture.from("images/character-sprites/Hannit_Sprite.png");
-export const olberic = PIXI.Texture.from("images/character-sprites/Olberic_Sprite.png");
-export const ophelia = PIXI.Texture.from("images/character-sprites/Ophelia_Sprite.png");
-export const primrose = PIXI.Texture.from("images/character-sprites/Primrose_Sprite.png");
-export const therion = PIXI.Texture.from("images/character-sprites/Therion_Sprite.png");
-export const tressa = PIXI.Texture.from("images/character-sprites/Tressa_Sprite.png");
+    constructor (charName) {
+
+        switch (charName) {
+            case "alfyn":
+                var sprite = new PIXI.Sprite.from("images/character-sprites/Alfyn_Sprite.png");
+                sprite.scale.x = .55;
+                sprite.scale.y = .55;
+                break;
+            case "cyrus":
+                var sprite = new PIXI.Sprite.from("images/character-sprites/Cyrus_Sprite.png");
+                sprite.scale.x = .55;
+                sprite.scale.y = .55;
+                break;
+            case "hannit":
+                var sprite = new PIXI.Sprite.from("images/character-sprites/Hannit_Sprite.png");
+                sprite.scale.x = .5;
+                sprite.scale.y = .5;
+                break;
+            case "olberic":
+                var sprite = new PIXI.Sprite.from("images/character-sprites/Olberic_Sprite.png");
+                sprite.scale.x = .54;
+                sprite.scale.y = .54;
+                break;
+            case "ophelia":
+                var sprite = new PIXI.Sprite.from("images/character-sprites/Ophelia_Sprite.png");
+                sprite.scale.x = .53;
+                sprite.scale.y = .53;
+                break;
+            case "primrose":
+                var sprite = new PIXI.Sprite.from("images/character-sprites/Primrose_Sprite.png");
+                sprite.scale.x = .54;
+                sprite.scale.y = .54;
+                break;
+            case "therion":
+                var sprite = new PIXI.Sprite.from("images/character-sprites/Therion_Sprite.png");
+                sprite.scale.x = .58;
+                sprite.scale.y = .58;
+                break;
+            case "tressa":
+                var sprite = new PIXI.Sprite.from("images/character-sprites/Tressa_Sprite.png");
+                sprite.scale.x = .54;
+                sprite.scale.y = .54;
+                break;
+    
+        }
+
+        this.getName = function() {
+            console.log(charName);
+            return charName;
+        }
+        this.getSprite = function() {
+            return sprite;
+        }
+    }
+
+}
